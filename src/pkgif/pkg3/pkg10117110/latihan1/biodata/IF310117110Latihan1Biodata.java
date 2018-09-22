6fcf3b7a6be5f185623124587a5f78c7d1377748
fauzisunarya <fauzialdzikirullah@gmail.com>
2018-09-22 16:16:36 +0700

    versi 1.0.0

---
+++ src/pkgif/pkg3/pkg10117110/latihan1/biodata/IF310117110Latihan1Biodata.java
@@ -0,0 +1,30 @@
+
+package pkgif.pkg3.pkg10117110.latihan1.biodata;
+
+/**
+ *
+ * @author
+ * Nama : Fauzi Sunarya
+ * Kelas : IF-3
+ * NIM : 10117110
+ * Deskripsi Program : Program Jenis ini Berisi Program Untuk Menampilkan
+ * Biodata
+ */
+public class IF310117110Latihan1Biodata {
+
+
+    public static void main(String[] args) {
+                
+        System.out.println("BIODATA" + "\n" + "------------------------------");
+        System.out.println("Nama\t\t: Fauzi Sunarya");
+        System.out.println("Alamat\t\t: Babakan Maja Rt/Rw 18/06 Indramayu");
+        System.out.println("Tempat Lahir\t: Indramayu");
+        System.out.println("Tanggal Lahir\t: 02 Juni 1999");
+        System.out.println("------------------------------");
+        System.out.println("Hoby\t\t: Membaca");
+        System.out.println("Makes\t\t: Seafod ");
+        System.out.println("Mikes\t\t: Milk ");
+        
+    }
+    
+}
